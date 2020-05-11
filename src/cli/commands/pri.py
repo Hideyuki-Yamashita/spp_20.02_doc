yasufum/spp-tmp

commit 427350f31cfec3b86995e1180a787c9d3eaf9a96

cli: support pipe PMD
This patch adds add pipe command to the primary. Information about pipe
is added to the output of status command of the primary too.

Signed-off-by: Itsuro Oda <oda@valinux.co.jp>
Acked-by: Yasufumi Ogawa <yasufum.o@gmail.com>

src/cli/commands/pri.py
@@ -229,6 +229,12 @@ class SppPrimary(object):
                     else:
                         print('    - {} -> {}'.format(port, dst))
 
+            if ('pipes' in json_obj):
+                print('  - pipes:')
+                for pipe in json_obj['pipes']:
+                    print('    - pipe:{} ring:{} ring:{}'.format(pipe['id'],
+                        pipe['rx'], pipe['tx']))
+
             if ('phy_ports' in json_obj) or ('ring_ports' in json_obj):
                 print('- stats')
 
@@ -798,6 +804,10 @@ class SppPrimary(object):
             print("'%s' is already added." % params[0])
         else:
             req_params = {'action': 'add', 'port': params[0]}
+            if len(params) == 3:
+                # add pipe:X ring:A ring:B
+                req_params['rx'] = params[1]
+                req_params['tx'] = params[2]
 
             res = self.spp_ctl_cli.put('primary/ports', req_params)
             if res is not None:

Hideyuki-Yamashita/spp_20.02_doc

commit 36ca950f639fc192a02bc1f1baabaadece82478e

(x-fn-spp-ml 679) [spp] [PATCH 1/2] cli: enable to add pipe without forwarder
Pipe port is independent of the forwarder but it can be added
only if the forwarder exists currently.
This patch enables to add pipe port even if the forwarder does
not exist.

Fixes: 427350f31cfe (cli: support pipe PMD)

Signed-off-by: Itsuro Oda <oda@valinux.co.jp>

src/cli/commands/pri.py
@@ -49,6 +49,9 @@ class SppPrimary(object):
 
         self.flow = SppPrimaryFlow(spp_ctl_cli)
 
+    def _port_is_pipe(self, params):
+        return len(params) > 0 and params[0].startswith("pipe:")
+
     def _do_if_forwarder_exists(self, status, func, params):
         """Execute command of func if forwarder thread is existing.
 
@@ -103,10 +106,16 @@ class SppPrimary(object):
                     print('Error: unknown response from status.')
 
         elif subcmd == 'add':
-            self._do_if_forwarder_exists(status, self._run_add, params)
+            if self._port_is_pipe(params):
+                self._run_add(params, is_pipe=True)
+            else:
+                self._do_if_forwarder_exists(status, self._run_add, params)
 
         elif subcmd == 'del':
-            self._do_if_forwarder_exists(status, self._run_del, params)
+            if self._port_is_pipe(params):
+                self._run_del(params, is_pipe=True)
+            else:
+                self._do_if_forwarder_exists(status, self._run_del, params)
 
         elif subcmd == 'forward' or subcmd == 'stop':
             self._do_if_forwarder_exists(status,
@@ -795,7 +804,7 @@ class SppPrimary(object):
             index += 1
         return opts_dict
 
-    def _run_add(self, params):
+    def _run_add(self, params, is_pipe=False):
         """Run `add` command."""
 
         if len(params) == 0:
@@ -819,12 +828,13 @@ class SppPrimary(object):
                 else:
                     print('Error: unknown response for add.')
 
-            self.ports = self._get_ports()  # update to current status
-            if self.ports is None:
-                print('Cannot retrieve ports from spp_primary')
-                self.ports = []
+            if not is_pipe:
+                self.ports = self._get_ports()  # update to current status
+                if self.ports is None:
+                    print('Cannot retrieve ports from spp_primary')
+                    self.ports = []
 
-    def _run_del(self, params):
+    def _run_del(self, params, is_pipe=False):
         """Run `del` command."""
 
         if len(params) == 0:
@@ -849,10 +859,11 @@ class SppPrimary(object):
                     else:
                         print('Error: unknown response for del.')
 
-            self.patches = self._get_patches()  # update to current status
-            if self.patches is None:
-                print('Cannot retrieve patches from spp_primary')
-                self.patches = []
+            if not is_pipe:
+                self.patches = self._get_patches()  # update to current status
+                if self.patches is None:
+                    print('Cannot retrieve patches from spp_primary')
+                    self.patches = []
 
     def _run_forward_or_stop(self, cmd):
         """Run `forward` or `stop` command."""