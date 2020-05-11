yasufum/spp-tmp

commit 427350f31cfec3b86995e1180a787c9d3eaf9a96

cli: support pipe PMD
This patch adds add pipe command to the primary. Information about pipe
is added to the output of status command of the primary too.

Signed-off-by: Itsuro Oda <oda@valinux.co.jp>
Acked-by: Yasufumi Ogawa <yasufum.o@gmail.com>

src/cli/commands/pri.py
@@ -229,6 +229,12 @@ class SppPrimary(object):
                     else:
                         print('    - {} -> {}'.format(port, dst))
 
+            if ('pipes' in json_obj):
+                print('  - pipes:')
+                for pipe in json_obj['pipes']:
+                    print('    - pipe:{} ring:{} ring:{}'.format(pipe['id'],
+                        pipe['rx'], pipe['tx']))
+
             if ('phy_ports' in json_obj) or ('ring_ports' in json_obj):
                 print('- stats')
 
@@ -798,6 +804,10 @@ class SppPrimary(object):
             print("'%s' is already added." % params[0])
         else:
             req_params = {'action': 'add', 'port': params[0]}
+            if len(params) == 3:
+                # add pipe:X ring:A ring:B
+                req_params['rx'] = params[1]
+                req_params['tx'] = params[2]
 
             res = self.spp_ctl_cli.put('primary/ports', req_params)
             if res is not None:

Hideyuki-Yamashita/spp_20.02_doc

commit 2f00a70d633f29ab86cd4a36819f8287a74e5efa

(x-fn-spp-ml 678) [spp] [PATCH 2/2] cli: fix status display of pipes
This patch fixes the indent of pipes in the status display of
the spp_primary. It should be the most left side because it
is a top attribute of json response.

Fixes: 427350f31cfe (cli: support pipe PMD)

Signed-off-by: Itsuro Oda <oda@valinux.co.jp>

src/cli/commands/pri.py
@@ -239,9 +239,9 @@ class SppPrimary(object):
                         print('    - {} -> {}'.format(port, dst))
 
             if ('pipes' in json_obj):
-                print('  - pipes:')
+                print('- pipes:')
                 for pipe in json_obj['pipes']:
-                    print('    - pipe:{} ring:{} ring:{}'.format(pipe['id'],
+                    print('  - pipe:{} ring:{} ring:{}'.format(pipe['id'],
                         pipe['rx'], pipe['tx']))
 
             if ('phy_ports' in json_obj) or ('ring_ports' in json_obj):